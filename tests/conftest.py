1155056a3f32504fda229d689be6f9cac2256f98
BRunknown <asafe.ferr@gmail.com>
2026-02-25 22:58:22 -0300

    <tests>: adicionando teste para o modulo storage.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,15 +3,6 @@ import httpx
 import respx
 
 
-@pytest.fixture
-def temp_folder(tmp_path):
-    # Criar estrutura de pastas
-    pasta_dados = tmp_path / "teste"
-    pasta_dados.mkdir()
-
-    yield pasta_dados
-
-
 @pytest.fixture(scope="session")
 @respx.mock
 def client():
@@ -19,3 +10,5 @@ def client():
         base_url="https://servicodados.ibge.gov.br/api/v1/localidades"
     )
     return client
+
+
